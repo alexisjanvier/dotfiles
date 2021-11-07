0cc6df101855606c805222f70beb2229f39f39a6
Alexis Janvier <contact@alexisjanvier.net>
2021-11-07 19:10:18 +0100

    Kitty and starship

--- install/install-cli-tools.sh
+++ install/install-cli-tools.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# starship minimal, blazing-fast, and infinitely customizable prompt
+# https://starship.rs/
+echo '[*] Install starship'
+sh -c "$(curl -fsSL https://starship.rs/install.sh)"
 # ls => lsd - https://github.com/Peltoche/lsd
 echo '[*] Install lsd to replace ls'
 cargo install lsd
--- install/stow-all.sh
+++ install/stow-all.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 echo '[*] Removing default configurations ...'
-rm rf ~/.local/share/fonts/DankMono ~/.config/lsd
+rm rf ~/.local/share/fonts/DankMono ~/.config/lsd ~/.config/kitty ~/.config/starship.toml
 
 echo '[*] Stowing/Creating simlinks for fonts'
 cd ../dotfiles && stow -vSt ~ fonts && cd ../install
+echo '[*] Stowing/Creating simlinks for kitty'
+cd ../dotfiles && stow -vSt ~ kitty && cd ../install
+echo '[*] Stowing/Creating simlinks for starship'
+cd ../dotfiles && stow -vSt ~ starship && cd ../install
 echo '[*] Stowing/Creating simlinks for lsd'
 cd ../dotfiles && stow -vSt ~ lsd && cd ../install
